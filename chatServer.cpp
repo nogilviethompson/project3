b260011de5b81dde48574597c9dd5f5b3453879b
Nicky Ogilvie-Thompson <nogilviethompson@gmail.com>
2017-02-23 10:40:05 -0500

    Committing the Server

--- chatServer.cpp
+++ chatServer.cpp
@@ -4,9 +4,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-<<<<<<< HEAD
 #include <vector>
-=======
 #include <iostream>
 #include <fstream>
 #include <map>
@@ -17,7 +15,6 @@
 #include "stemming.h"
 #include "string_util.h"
 #include "utilities.h"
->>>>>>> 524843d833cf43c1251f66dc14b18852063556de
 #include "fifo.h"
 
 using namespace std;
@@ -26,7 +23,6 @@ using namespace std;
 string receive_fifo = "messageRequest";
 string send_fifo = "messageReply";
 
-<<<<<<< HEAD
 int main() {
   vector<string> chatLog;
   string username, message, full, outMessage;
@@ -36,13 +32,7 @@ int main() {
   Fifo sendfifo(send_fifo); 
   
   chatLog.push_back(" ");
-=======
-/* Server main line,create name MAP, wait for and serve requests */
-int main() {
-  vector<string> chatLog;
-  string username, message, full;
   
->>>>>>> 524843d833cf43c1251f66dc14b18852063556de
   while (1) {
 
     /* Get a message from a client */
@@ -58,24 +48,15 @@ int main() {
 	
 	//Create a string that is the message term
 	message = inMessage.substr(pos,2000);
-	cout << "Received: " << username << " - " << message << endl;
-	
+	cout << "Received: " << username << " - " << message << endl;	
 	full = username + ": " + message;
-	
-<<<<<<< HEAD
 	chatLog.push_back(full);
-=======
-	chatLog.pushback(full);
->>>>>>> 524843d833cf43c1251f66dc14b18852063556de
 	
 	//Iterate through the positions vector, sending each line out as a message
 	for (unsigned int i = 0; i < chatLog.size(); i++)
     {
-<<<<<<< HEAD
     outMessage = chatLog[i];
-=======
     string outMessage = positions[i];
->>>>>>> 524843d833cf43c1251f66dc14b18852063556de
 	cout << outMessage << endl;
 	sendfifo.openwrite();
 	sendfifo.send(outMessage);
@@ -84,10 +65,7 @@ int main() {
 	
 	//After all the messages have been sent, send out the $END signal
 	outMessage = "$END";
-<<<<<<< HEAD
 	cout << outMessage << endl;
-=======
->>>>>>> 524843d833cf43c1251f66dc14b18852063556de
 	sendfifo.openwrite();
 	sendfifo.send(outMessage);
 	sendfifo.fifoclose();
